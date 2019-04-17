8e1803eb838ddd9a8fba4d7d70d7027c164642f7
AnirudhRahul <anirudhrahul@anjurahul.com>
2019-04-17 02:32:51 -0400

    Restructured project code

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 var express = require('express');
 app = express();
 port = process.env.PORT;
+
+var usersRoute = require('./routes/users');
+app.use('/users', usersRoute);
+
 app.listen(port);
 console.log('server started on: ' + port);
-var router = require("../router.js");
-app.use(router);
---
+++ routes/users.js
@@ -0,0 +1,36 @@
+module.exports = (function() {
+    'use strict';
+
+    var users = require('express').Router();
+    var mongoose = require('mongoose'),
+    User = mongoose.model('User');
+    //TODO implement api responeses
+
+    users.get('/', function (req, res) {
+      res.send("get_user route is working");
+      User.findOne({ email: req.query.email, password: req.query.password }, function (err, user) {
+          if(err)
+            res.send(err);
+          else
+            res.json(user);
+      });
+    });
+
+
+     users.put('/', function (req, res) {
+       res.send("add_user route is working");
+       var newUser = new User({
+           displayName=req.body.displayName,
+           email=req.body.email,
+           password=req.body.password
+       });
+       newUser.save({}, function(err, user) {
+         if (err)
+           res.send(err);
+         else
+           res.json(user);
+       });
+     });
+
+    return users;
+})();
