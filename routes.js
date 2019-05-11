0ad13432e9b8cb030e4a49d28fa2f1e9c67e1adb
AnirudhRahul <anirudhrahul@anjurahul.com>
2019-05-11 15:44:05 -0400

    Fixed path to controller

--- routes.js
+++ routes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const userController = require('../controllers/userController.js')
+const userController = require('./controllers/userController.js')
 const router = express.Router()
 
 router.post('/user', userController.add_user)
