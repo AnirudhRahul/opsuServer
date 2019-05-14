828489385c2a0fbf3e215caa1511df62f419877e
AnirudhRahul <anirudhrahul@anjurahul.com>
2019-05-14 02:06:22 -0400

    Set all user fields as required

--- models/User.js
+++ models/User.js
@@ -1,12 +1,10 @@
 var mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-  email: {type: String, index: true, unique: true},
-  password: String,
-  displayName: {type: String, index: true, unique: true}
+  email: {type: String, index: true, unique: true, required: true},
+  password: {type: String, required: true},
+  displayName: {type: String, index: true, unique: true, required: true}
 });
 
-//Sets every attribute as reqiored
-_.each(_.keys(schema), function (attr) { schema[attr].required = true; });
 //Registers the model with mongoose and exports it
 module.exports = mongoose.model('User', schema);
