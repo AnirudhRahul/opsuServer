61fea6df898e51a1af9039feac54248706da1bd6
AnirudhRahul <anirudhrahul@anjurahul.com>
2019-05-19 16:39:30 -0400

    Added routes for friend systems

--- routes.js
+++ routes.js
@@ -2,7 +2,16 @@ const express = require('express')
 const userController = require('./controllers/userController.js')
 const router = express.Router()
 
-router.post('/user', userController.add_user)
-router.get('/user', userController.get_user)
+router.route('/user')
+  .post(userController.add_user)
+  .get(userController.get_user);
+
+router.route('/user/friends')
+  .get(userController.get_friends);
+
+router.route('/user/friendRequest')
+  .get(userController.get_friendRequests)
+  .post(userController.create_request)
+  .delete(userController.delete_request);
 
 module.exports = router
