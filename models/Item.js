1285c43292f8de307ed8a8b371854849bf4c1997
AnirudhRahul <anirudhrahul@anjurahul.com>
2019-06-01 14:40:09 -0400

    Created model for purchasable items

---
+++ models/Item.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var conn = mongoose.createConnection('mongodb://localhost:27017/opsu_Items', {useNewUrlParser: true});
+
+const schema = new mongoose.Schema({
+  name: {type: String, unique: true, required: true},
+  cost: {type: Number}
+});
+
+//Registers the model with mongoose and exports it
+module.exports = conn.model('Item', schema);
