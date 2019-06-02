cd3bc43eac881aaa46424cadafa9ed8c3a6e8b61
AnirudhRahul <anirudhrahul@anjurahul.com>
2019-06-02 15:01:39 -0400

    Created beatmap model

--- models/Beatmap.js
+++ models/Beatmap.js
@@ -1,9 +1,35 @@
 var mongoose = require('mongoose');
+var conn = mongoose.createConnection('mongodb://localhost:27017/opsu_Beatmaps', {useNewUrlParser: true});
 
-const schema = new Schema({
-  displayName: String,
-  score: String,
-  email: String
+const schema = new mongoose.Schema({
+  username:   String,
+  beatmapID:  Number,
+  setID:      Number,
+  title:      String,
+  creator:    String,
+  artist:     String,
+  version:    String,
+  hit300:     Number,
+  hit100:     Number,
+  hit50:      Number,
+  geki:       Number,
+  katu:       Number,
+  miss:       Number,
+  score:      Number,
+  combo:      Number,
+  perfect:    Boolean,
+  mods:       Number,
+  time:       Number
+
 });
 
-module.exports = mongoose.model('Beatmap', schema);
+//Make all fields required
+for(var i in schema.paths){
+  schema.paths[i].required(true);
+}
+
+schema.index({ beatmapID: 1, score: -1, username: 1 }); // schema level
+
+
+
+module.exports = conn.model('Beatmap', schema);
