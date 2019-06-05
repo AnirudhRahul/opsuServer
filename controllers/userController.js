7ab5a6f6953fec7900979380cbaee1f4d1406ee4
AnirudhRahul <anirudhrahul@anjurahul.com>
2019-06-05 19:44:05 -0400

    Fixed parenthesis, brackets, and styling

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,77 +6,77 @@ User = require('../models/User.js')
 
 //GET methods
 exports.get_user = function(req, res) {
-User.findOne({
-  displayName: req.query.displayName,
-  password: req.query.password
-}, function(err, user) {
-  if (err)
-    res.status(400).send(err);
-  else if (user)
-    res.send(user);
-  else
-    res.status(404).send('Username or password incorrect');
+  User.findOne({
+    displayName: req.query.displayName,
+    password: req.query.password
+  }, function(err, user) {
+    if (err)
+      res.status(400).send(err);
+    else if (user)
+      res.send(user);
+    else
+      res.status(404).send('Username or password incorrect');
 
-  console.log("End reached");
-});
+    console.log("End reached");
+  });
 
-};
+}
 
 exports.get_friendRequests = function(req, res) {
-User.findOne({
-  displayName: req.query.displayName
-}, 'friend_requests', function(err, user) {
-  if (err)
-    res.status(400).send(err);
-  else if (user)
-    res.send(user);
-  else
-    res.status(404).send('User not found');
-});
+  User.findOne({
+    displayName: req.query.displayName
+  }, 'friend_requests', function(err, user) {
+    if (err)
+      res.status(400).send(err);
+    else if (user)
+      res.send(user);
+    else
+      res.status(404).send('User not found');
+  });
 
-};
+}
 
 exports.get_friends = function(req, res) {
 
-User.findOne({
-  displayName: req.query.displayName
-}, 'friends', function(err, user) {
-  if (err)
-    res.status(400).send(err);
-  else if (user)
-    res.send(user);
-  else
-    res.status(404).send('User not found');
-});
-
-};
-
-exports.create_request = function(req, res) {
-const friendName = req.query.friendName;
-const self = req.query.displayName;
-
-User.findOneAndUpdate({
-    displayName: friendName
-  }, {
-    $addToSet: {
-      friend_requests: self
-    }
-  },
-  function(err, doc) {
+  User.findOne({
+    displayName: req.query.displayName
+  }, 'friends', function(err, user) {
     if (err)
       res.status(400).send(err);
-    else if (doc)
-      res.send("Friend Request sent");
+    else if (user)
+      res.send(user);
     else
-      res.status(404).send("Friend name doesn't exist");
-
+      res.status(404).send('User not found');
   });
 
-};
+}
 
-function addFriend(self, friend) {
-//Add eachother to friends lists
-return new Promise(function(resolve, reject) {
+exports.create_request = function(req, res) {
+  const friendName = req.query.friendName;
+  const self = req.query.displayName;
+
+  User.findOneAndUpdate({
+      displayName: friendName
+    }, {
+      $addToSet: {
+        friend_requests: self
+      }
+    },
+    function(err, doc) {
+      if (err)
+        res.status(400).send(err);
+      else if (doc)
+        res.send("Friend Request sent");
+      else
+        res.status(404).send("Friend name doesn't exist");
+
+    });
+
+}
+
+var addFriend = function(self, friend) {
+  //Add eachother to friends lists
+  return new Promise(function(resolve, reject) {
     User.findOneAndUpdate({
         displayName: self
       }, {
@@ -90,8 +90,8 @@ return new Promise(function(resolve, reject) {
         else {
           resolve();
         }
-      });
-  }
+      })
+  });
 }
 
 function friendLimitReached(self) {
@@ -108,76 +108,75 @@ function friendLimitReached(self) {
         reject(new Error(self + ' Friend limit reached'))
       else
         resolve();
-    });
-
-  })
+    })
+  });
 
 }
 
 function deleteFriendRequest(self, friendName) {
   return new Promise(function(resolve, reject) {
-      User.findOneAndUpdate({
-          displayName: self
-        }, {
-          $pull: {
-            friend_requests: friendName
-          }
-        },
-        function(err, doc) {
-          if (err)
-            reject(err);
-          else
-            resolve();
+    User.findOneAndUpdate({
+        displayName: self
+      }, {
+        $pull: {
+          friend_requests: friendName
+        }
+      },
+      function(err, doc) {
+        if (err)
+          reject(err);
+        else
+          resolve();
 
-        });
-    }
-  }
+      })
+  });
+}
 
 
-  exports.delete_request = function(req, res) {
-    const friendName = req.query.friendName;
-    const self = req.query.displayName;
-    const accept = req.query.accept == 'true';
-    var worked = true;
-    if (accept) {
-      Promise.all([friendLimitReached(self), friendLimitReached(friendName)])
-        .then(Promise.all([addFriend(self, friendName), addFriend(friendName, self)]))
-        .then(deleteFriendRequest(self, friendName)).then(
-          function() {
-            res.send("Friend Request accepted");
-          },
-          function(err) {
-            res.status(400).send(err);
-          }
-        );
-
-    } else {
-      deleteFriendRequest(self, friendName).then(
+exports.delete_request = function(req, res) {
+  const friendName = req.query.friendName;
+  const self = req.query.displayName;
+  const accept = req.query.accept == 'true';
+  var worked = true;
+  if (accept) {
+    Promise.all([friendLimitReached(self), friendLimitReached(friendName)])
+      .then(Promise.all([addFriend(self, friendName), addFriend(friendName, self)]))
+      .then(deleteFriendRequest(self, friendName)).then(
         function() {
-          res.send("Friend Request deleted");
+          res.send("Friend Request accepted");
         },
         function(err) {
           res.status(400).send(err);
         }
       );
-    }
 
-  };
-
-
-  exports.add_user = function(req, res) {
-
-    var newUser = new User({
-      displayName: req.body.displayName,
-      email: req.body.email,
-      password: req.body.password
-    });
-
-    newUser.save({}, function(err, user) {
-      if (err)
+  } else {
+    deleteFriendRequest(self, friendName).then(
+      function() {
+        res.send("Friend Request deleted");
+      },
+      function(err) {
         res.status(400).send(err);
-      else
-        res.send(user);
-    });
+      }
+    );
+  }
 
-  };
+};
+
+
+exports.add_user = function(req, res) {
+
+  var newUser = new User({
+    displayName: req.body.displayName,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  newUser.save({}, function(err, user) {
+    if (err)
+      res.status(400).send(err);
+    else
+      res.send(user);
+  });
+
+};
