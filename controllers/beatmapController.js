91bac011c93a82c562b73a75d4f094199caddffe
AnirudhRahul <anirudhrahul@anjurahul.com>
2019-06-05 21:00:42 -0400

    Beatified controlle code

--- controllers/beatmapController.js
+++ controllers/beatmapController.js
@@ -1,24 +1,22 @@
 var mongoose = require('mongoose'),
-Beatmap = require('../models/Beatmap.js')
+  Beatmap = require('../models/Beatmap.js')
 
 //TODO implement api responeses
 
 exports.get_leaderboard = function(req, res) {
-  if(req.query.type=='top'){
+  if (req.query.type == 'top') {
     start = 0
-    if(req.query.start)
-      start=req.query.start;
+    if (req.query.start)
+      start = req.query.start;
     Beatmap.findOneAndUpdate({
       beatmapID: req.query.email,
-    },
-    {
-        skip:start, // Starting Row
-        limit:100, // Ending Row
-        sort:{
-            score: -1 //Sort by Score descending
-        }
-    }
-    ,function(err, user) {
+    }, {
+      skip: start, // Starting Row
+      limit: 100, // Ending Row
+      sort: {
+        score: -1 //Sort by Score descending
+      }
+    }, function(err, user) {
       if (err)
         res.status(400).send(err);
       else if (user)
@@ -27,32 +25,31 @@ exports.get_leaderboard = function(req, res) {
         res.status(404).send('User not found');
     });
 
-  }
-  else if(req.query.type=='my'){
+  } else if (req.query.type == 'my') {
 
   }
 };
 
 exports.add_to_leaderboard = function(req, res) {
   var newScore = new Beatmap({
-    username:   req.body.username,
-    beatmapID:  req.body.beatmapID,
-    setID:      req.body.setID,
-    title:      req.body.title,
-    creator:    req.body.creator,
-    artist:     req.body.artist,
-    version:    req.body.version,
-    hit300:     req.body.hit300,
-    hit100:     req.body.hit100,
-    hit50:      req.body.hit50,
-    geki:       req.body.geki,
-    katu:       req.body.katu,
-    miss:       req.body.miss,
-    score:      req.body.score,
-    combo:      req.body.combo,
-    perfect:    req.body.perfect,
-    mods:       req.body.mods,
-    time:       req.body.time
+    username: req.body.username,
+    beatmapID: req.body.beatmapID,
+    setID: req.body.setID,
+    title: req.body.title,
+    creator: req.body.creator,
+    artist: req.body.artist,
+    version: req.body.version,
+    hit300: req.body.hit300,
+    hit100: req.body.hit100,
+    hit50: req.body.hit50,
+    geki: req.body.geki,
+    katu: req.body.katu,
+    miss: req.body.miss,
+    score: req.body.score,
+    combo: req.body.combo,
+    perfect: req.body.perfect,
+    mods: req.body.mods,
+    time: req.body.time
   });
 
   newScore.save({}, function(err, beatmap) {
