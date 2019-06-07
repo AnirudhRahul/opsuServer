504f9ed38eb135695a7c9f19247f91a472e00377
AnirudhRahul <anirudhrahul@anjurahul.com>
2019-06-06 21:38:29 -0400

    Fixed nodemail syntax error

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 User = require('../models/User.js')
 
 const nodemailer = require('nodemailer');
-let transporter = nodeMailer.createTransport({
+let transporter = nodemailer.createTransport({
   host: 'email-smtp.us-east-1.amazonaws.com',
   port: 465,
   secure: true,
