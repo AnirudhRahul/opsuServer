c89004265cfba18965d0f4c157e26f23e8cf2e75
AnirudhRahul <anirudhrahul@anjurahul.com>
2019-06-27 23:08:27 -0400

    Used proper piping methods

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -5,21 +5,17 @@ User = require('../models/User.js')
 exports.get_all_users = function(req, res) {
 	if (req.body.admin !== process.env.ADMIN_KEY)
 		return;
-	var stream = User.find().cursor();
-	stream.pipe(res);
-	stream.on('end', function() {
-		res.end();
-	});
-
+	var cursor = User.find().cursor();
+	//pipes output through res
+	cursor.pipe(JSONStream.stringify())
+		.pipe(res.type('json'));
 }
 
 exports.get_all_field = function(req, res) {
 	if (req.body.admin !== process.env.ADMIN_KEY)
 		return;
-	var stream = User.find().cursor();
-	stream.pipe(res);
-	stream.on('end', function() {
-		res.end();
-	});
-
+	var cursor = User.find().cursor();
+	//pipes output through res
+	cursor.pipe(JSONStream.stringify())
+		.pipe(res.type('json'));
 }
