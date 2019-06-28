1a781480535e98e79f1fdccc00a13059933c106a
AnirudhRahul <anirudhrahul@anjurahul.com>
2019-06-28 13:11:47 -0400

    Added filter for retrieving single fields

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -7,15 +7,17 @@ exports.get_all_users = function(req, res) {
 		return;
 	var cursor = User.find().cursor();
 	//pipes output through res
-	cursor.pipe(JSONStream.stringify())
-		.pipe(res.type('json'));
+	cursor.pipe(JSONStream.stringify()).pipe(res.type('json'));
 }
 
 exports.get_all_field = function(req, res) {
 	if (req.body.admin !== process.env.ADMIN_KEY)
 		return;
-	var cursor = User.find().cursor();
-	//pipes output through res
-	cursor.pipe(JSONStream.stringify())
-		.pipe(res.type('json'));
+	try {
+		var cursor = User.find({}, req.body.field, {}).cursor
+		cursor.pipe(JSONStream.stringify()).pipe(res.type('json'));
+	}
+	catch (err) {
+		res.status(200).send(err);
+	}
 }
