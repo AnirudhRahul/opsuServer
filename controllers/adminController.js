75864653ed4773cfa3a32be8540c7b99a116e200
AnirudhRahul <anirudhrahul@anjurahul.com>
2019-06-27 22:40:22 -0400

    Switched to cursors instead of deprecated streams

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,23 +3,23 @@ User = require('../models/User.js')
 
 
 exports.get_all_users = function(req, res) {
-  if(req.body.admin!==process.env.ADMIN_KEY)
-    return;
-  var stream = User.find().stream();
-  stream.pipe(res);
-  readStream.on('end', function(){
-      res.end();
-  });
+	if (req.body.admin !== process.env.ADMIN_KEY)
+		return;
+	var stream = User.find().cursor();
+	stream.pipe(res);
+	readStream.on('end', function() {
+		res.end();
+	});
 
 }
 
-exports.get_all_field = function(req, res){
-  if(req.body.admin!==process.env.ADMIN_KEY)
-    return;
-  var stream = User.find({},req.body.field).stream();
-  stream.pipe(res);
-  readStream.on('end', function(){
-      res.end();
-  });
+exports.get_all_field = function(req, res) {
+	if (req.body.admin !== process.env.ADMIN_KEY)
+		return;
+	var stream = User.find().cursor();
+	stream.pipe(res);
+	readStream.on('end', function() {
+		res.end();
+	});
 
 }
