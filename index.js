9c2cd5c9c454c185ab46f9f5b84b20660088da59
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-04-29 18:24:57 -0400

    Commented out https/ssl keys Added comment for JWT token auth

--- index.js
+++ index.js
@@ -9,27 +9,28 @@ const https = require('https');
 const http = require('http');
 
 const fs = require('fs');
-const privateKey  = fs.readFileSync(__dirname + '/ssl.key');
-const certificate = fs.readFileSync(__dirname + '/ssl.crt');
-const credentials = {key: privateKey, cert: certificate};
+// const privateKey  = fs.readFileSync(__dirname + '/ssl.key');
+// const certificate = fs.readFileSync(__dirname + '/ssl.crt');
+// const credentials = {key: privateKey, cert: certificate};
 
 
 app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app2 = express();
-app2.use(bodyParser.json());
-app2.use(bodyParser.urlencoded({ extended: true }));
+// app2 = express();
+// app2.use(bodyParser.json());
+// app2.use(bodyParser.urlencoded({ extended: true }));
 
 const routes = require('./routes.js');
+// const authentication = require("./middleware/auth")
 app.use('/test', routes);
 
-const routes2 = require('./resetRoutes.js');
-app2.use(routes2);
+// const routes2 = require('./resetRoutes.js');
+// app2.use(routes2);
 
-const httpsServer = https.createServer(credentials, app);
-const httpServer = http.createServer(app2);
+// const httpsServer = https.createServer(credentials, app);
+const httpServer = http.createServer(app);
 
-httpsServer.listen(process.env.PORT);
-httpServer.listen(80)
+// httpsServer.listen(process.env.PORT);
+httpServer.listen(process.env.PORT | 3000)
